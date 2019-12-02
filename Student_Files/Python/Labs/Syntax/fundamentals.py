a8575f4ca65a47c02fcb581a48a5fc465ae9ca9d
Kevin J Donohue <kevinjdonohue@yahoo.com>
2019-12-02 12:44:27 -0800

    Fixed version of the fundamentals lab

--- Student_Files/Python/Labs/Syntax/fundamentals.py
+++ Student_Files/Python/Labs/Syntax/fundamentals.py
@@ -1,16 +1,17 @@
 # fundamentals.py
-The syntax of this program needs to be repaired in order for it to execute.
+# The syntax of this program needs to be repaired in order for it to execute.
 #
 
+from datetime import date
 x = 6
 y = "7"
-z = x times y
+z = x * y
 
 print("z = ", z)
 
 if z == 42:
-    z = z div 3
-        print("z = " - z)
+    z = z / 3
+    print("z = " - z)
 
 input("Enter a new value for x: ")
 input("Enter a new value for y: ")
@@ -22,13 +23,11 @@ print("z = " + z)
 Import the datetime module
 '''
 
-import date from datetime
 
-today = datetime.date.today()
+today = date.today()
 
 print("Today is ", today)
 print("The day of the month is ", today.day)
 print("The month of the year is ", today.month)
 print("The year is ", today.year)
 print("The day of the week is ", today.weekday())
-
