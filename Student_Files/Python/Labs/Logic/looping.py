7d8663c7d7a8f919bc5f42f1f7c9d4f3e0314f2a
Kevin J Donohue <kevinjdonohue@yahoo.com>
2019-12-02 14:56:15 -0800

    Updating the solutions for the labs

--- Student_Files/Python/Labs/Logic/looping.py
+++ Student_Files/Python/Labs/Logic/looping.py
@@ -1,5 +1,5 @@
 for nbr in range(1, 100):
-    if not nbr % 10 == 0:
+    if not nbr % 5 == 0 and not nbr % 10 == 0:
         print(nbr, end="  ")
     elif nbr % 10 == 0:
         print("\n")
--- Student_Files/Python/Labs/Logic/parsing.py
+++ Student_Files/Python/Labs/Logic/parsing.py
@@ -1,18 +1,14 @@
 nbrS1 = "12|23|72|238|515|7|23|723|2|5|73|567|7|33|6|14"
 nbrS2 = "72|28|spam|82|75|72|3|abc|27|25|2|7|71|def|6|14"
 
-s1_numbers = nbrS1.split("|")
-s2_numbers = nbrS2.split("|")
 
-
-def compute_total(string_of_numbers):
+def compute_total(pipe_delimited_numbers):
+    numbers = pipe_delimited_numbers.split("|")
     total = 0
-
-    numbers = string_of_numbers.split("|")
 
     for nbr in numbers:
         if nbr.isdigit():
-            total = total + int(nbr)
+            total += int(nbr)
 
     return total
 
